660b62ca768e65ee7e21419db86abadb02192876
dozenclub <80806789+dozenclub@users.noreply.github.com>
2021-03-22 17:18:49 +1100

    update pages

--- trade/tradeScript.js
+++ trade/tradeScript.js
@@ -1,37 +1,44 @@
 // 타자 데이터 샘플
-var rosterHitterData = [
-    ["김상수", "C", "깡", 400, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["찬물택", "1B", "달", 420, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["눕동님", "2B", "놉", 430, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["김도망", "SS", "비", 450, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["양준혁", "C", "쌤", 400, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["벤치성", "C", "쌤", 480, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["벤치성", "C", "요", 480, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["김타자", "C", "FA", 480, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
-    ["박타자", "C", "FA", 480, -20, 30, 0.96, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+var tradeHitterData = [
+    ["김상수", "C", "깡", 400, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["찬물택", "1B", "달", 420, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["눕동님", "2B", "놉", 430, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["김도망", "SS", "비", 450, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["양준혁", "C", "쌤", 400, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["벤치성", "C", "쌤", 480, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["벤치성", "C", "요", 480, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
+    ["김타자", "C", "FA", 480, -20, 0.300, 12,11,10,9,8,7,6,5,4,3,2,1],
+    ["박타자", "C", "FA", 480, -20, 0.300, 1,2,3,4,5,6,7,8,9,10,11,12],
 ]
-var rosterPitcherData = [
-    ["박찬호", "P", "깡", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
-    ["안지만", "P", "달", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
-    ["오승환", "P", "놉", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
-    ["벤치성", "P", "놉", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
-    ["배영수", "P", "요", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
-    ["정투수", "P", "FA", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
-    ["조투수", "P", "FA", 430, 10, 20, 0.94, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4]
+var tradePitcherData = [
+    ["박찬호", "P", "깡", 430, 10, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
+    ["안지만", "P", "달", 430, 10, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
+    ["오승환", "P", "놉", 430, 10, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
+    ["벤치성", "P", "놉", 430, 10, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
+    ["배영수", "P", "요", 430, 10, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
+    ["정투수", "P", "FA", 430, 10, 1.20, 0.45, 1,2,3,4,5,6,7, 2.17, 1,2,3,4],
+    ["조투수", "P", "FA", 430, 10, 1.20, 0.45, 7,6,5,4,3,2,1, 2.17, 4,3,2,1]
 ]
 
 // 전역 변수
-var hitterDefaultIndexRange   = [0,  6];
-var hitterAdvancedIndexRange  = [7, 19];
-var pitcherDefaultIndexRange  = [0,  6];
-var pitcherAdvancedIndexRange = [7, 20];
-var hitterAdvancedDataDecimals = [3,0,0,0,0,0,0,0,0,0,0,0,0];
-var pitcherAdvancedDataDecimals = [2,2,0,0,0,0,0,0,0,2,0,0,0,0];
-var hitterPositionAll = ["C", "1B", "2B", "3B", "SS", "LF", "RF", "CF", "RF", "DH"];
+var hitterBodyIndexRange    = [5, 17];
+var pitcherBodyIndexRange   = [5, 18];
+var hitterBodyDataDecimals  = [3,0,0,0,0,0,0,0,0,0,0,0,0];
+var pitcherBodyDataDecimals = [2,2,0,0,0,0,0,0,0,2,0,0,0,0];
+var hitterPositionAll           = ["C", "1B", "2B", "3B", "SS", "LF", "RF", "CF", "RF", "DH"];
+var hitterBodySortOrder     = [1,1,1,1,1,
+                               1,1,1,1,1,
+                               1,1,1,0,0]; // 0 = ascending, 1 = descending
+var pitcherBodySortOrder    = [1,1,0,0,1,
+                               1,1,0,1,1,
+                               1,1,0,0,0,
+                               1]; // 0 = ascending, 1 = descending
 
-var currentTeamSelection = [];
-var currentPosition = 0;
+var currentTeamSelection = ["FA"];
+var currentPosition = 2;
 var currentHitterPositionSelection = [];
+var currentSortIndex = 0;
+var currentPlayerIndices = [];
 
 function updateTeamSelection(string) {
 
@@ -45,7 +52,8 @@ function updateTeamSelection(string) {
     }
 
     // refresh page
-    refreshPlayerList();
+    updateCurrentPlayerList();
+    writePlayerTable();
 }
 
 function updatePosition(n) {
@@ -58,24 +66,22 @@ function updatePosition(n) {
     currentPosition = n;
 
     // initialize table status
-    rosterHitterDefault.hidden = true;
-    rosterHitterAdvanced.hidden = true;
-    rosterPitcherDefault.hidden = true;
-    rosterPitcherAdvanced.hidden = true;
+    rosterHitterFrame.hidden = true;
+    rosterPitcherFrame.hidden = true;
 
     // display default table for the position
     if(currentPosition == 1) {
-        rosterHitterDefault.hidden = false;
+        rosterHitterFrame.hidden = false;
         hitterPositionSelectionBar.hidden = false;
         updateHitterPositionSelection('All');
     } else {
-        rosterPitcherDefault.hidden = false;
+        rosterPitcherFrame.hidden = false;
         hitterPositionSelectionBar.hidden = true;
     }
 
     // refresh page
-    refreshPlayerList();
-
+    updateCurrentPlayerList();
+    writePlayerTable();
 }
 
 function updateHitterPositionSelection(string) {
@@ -87,153 +93,157 @@ function updateHitterPositionSelection(string) {
     if(string == 'All') {
         // first undo all selection
         for(i=0; i<currentHitterPositionSelection.length; i++) {
-            $('#hitterPosition'+currentHitterPositionSelection[i]).removeClass("btn-primary").addClass("btn-outline-primary");
+            $('#hitterPosition'+currentHitterPositionSelection[i]).removeClass("btn-dark").addClass("btn-outline-dark");
         }
+        currentHitterPositionSelection = [];
         // then select all
         for(i=0; i<hitterPositionAll.length; i++) {
             currentHitterPositionSelection.push(hitterPositionAll[i]);
-            $('#hitterPosition'+hitterPositionAll[i]).removeClass("btn-outline-primary").addClass("btn-primary");
+            $('#hitterPosition'+hitterPositionAll[i]).removeClass("btn-outline-dark").addClass("btn-dark");
         }
     } else {
         // toggle selection
         if(currentHitterPositionSelection.includes(string)) {
             currentHitterPositionSelection = currentHitterPositionSelection.filter(e => e != string);
-            $('#hitterPosition'+string).removeClass("btn-primary").addClass("btn-outline-primary");
+            $('#hitterPosition'+string).removeClass("btn-dark").addClass("btn-outline-dark");
         } else {
             currentHitterPositionSelection.push(string);
-            $('#hitterPosition'+string).removeClass("btn-outline-primary").addClass("btn-primary");
+            $('#hitterPosition'+string).removeClass("btn-outline-dark").addClass("btn-dark");
         }
+    }
+
+    // update hitter position button text
+    if(currentHitterPositionSelection.length == 10) {
+        // if all are selected, print "전부"
+        hitterPositionButton.innerHTML = "포지션: 전부";
+    } else {
+        hitterPositionButton.innerHTML = "포지션: " + currentHitterPositionSelection.join(", ");
     }
 
     // refresh page
-    refreshPlayerList();
-
+    updateCurrentPlayerList();
+    writePlayerTable();
 }
 
-function createRosterLineDefault(stringArray) {
-    var innerHTMLString = '<tr><th scope="row">' + stringArray[0] + '</th><td>' + stringArray[1] + '</td><td>' + stringArray[2] + '</td><td>' + stringArray[3] + '</td><td>';
-    if(stringArray[2] >= 0) {
-        innerHTMLString = innerHTMLString + '<span style="color:red;">&#9650;' + stringArray[4] + '</span></td><td>';
-    } else {
-        innerHTMLString = innerHTMLString + '<span style="color:blue;">&#9660;' + (-1 * Number(stringArray[4])) + '</span></td><td>';
+function updateCurrentPlayerList() {
+    
+    // auxiliary variables
+    var i;
+    var sortType = 0;
+    var currentPlayerArray = [];
+
+    // filter player indices
+    if(currentPosition == 1) { // if hitter
+        for(i=0; i<tradeHitterData.length; i++) {
+            if(currentTeamSelection.includes(tradeHitterData[i][2])) { // check team selection condition
+                if(currentHitterPositionSelection.includes(tradeHitterData[i][1])) { // check hitter position selection condition
+                    currentPlayerArray.push([i, tradeHitterData[i][3+currentSortIndex]]); // record player index and value for sorting
+                }
+            }
+        }
+        sortType = hitterBodySortOrder[currentSortIndex]; // record sort order
+    } else { // if pitcher
+        for(i=0; i<tradePitcherData.length; i++) {
+            if(currentTeamSelection.includes(tradePitcherData[i][2])) { // check team selection condition
+                currentPlayerArray.push([i, tradePitcherData[i][3+currentSortIndex]]); // record player index and value for sorting
+            }
+        }
+        sortType = pitcherBodySortOrder[currentSortIndex]; // record sort order
     }
-    if(stringArray[3] >= 0) {
-        innerHTMLString = innerHTMLString + '<span style="color:red;">&#9650;' + stringArray[5] + '</span></td><td>';
+
+    // sort player indices
+    if(sortType == 0) {
+        currentPlayerArray = currentPlayerArray.sort(function(a,b) {return a[1]-b[1]});
     } else {
-        innerHTMLString = innerHTMLString + '<span style="color:blue;">&#9660;' + (-1 * Number(stringArray[5])) + '</span></td><td>';
+        currentPlayerArray = currentPlayerArray.sort(function(a,b) {return b[1]-a[1]});
     }
-    innerHTMLString = innerHTMLString + (100 * Number(stringArray[6]).toFixed(2)) + '%</td></tr>';
-    return innerHTMLString;
+    
+    // replace current player indices
+    currentPlayerIndices = [];
+    for(i=0; i<currentPlayerArray.length; i++) {
+        currentPlayerIndices.push(currentPlayerArray[i][0]);
+    }
 }
 
-function refreshPlayerList() {
+function updateSortingVariable(n) {
+    currentSortIndex = n;
+    updateCurrentPlayerList();
+    writePlayerTable();
+}
+
+function writePlayerTable() {
 
     // initialize
-    var playerList = [];
     var i;
     var j;
-    
-    // if hitter is selected
+    var innerHTMLString;
+    var innerHTMLStringArray;
+    var innerHTMLStringArraySlice;
+    rosterHitterHeadBody.innerHTML   = '';
+    rosterHitterMainBody.innerHTML   = '';
+    rosterPitcherHeadBody.innerHTML   = '';
+    rosterPitcherMainBody.innerHTML   = '';
+
     if(currentPosition == 1) {
-
-        // filter hitters
-        for(i=0; i<rosterHitterData.length; i++) {
-            // check team selection
-            if(currentTeamSelection.includes(rosterHitterData[i][2])) {
-                // check position selection
-                if(currentHitterPositionSelection.includes(rosterHitterData[i][1])) {
-                    playerList.push(rosterHitterData[i]);
+        // update hitter tables
+        for(j=0; j<currentPlayerIndices.length; j++) {
+            // read hitter data
+            innerHTMLStringArray = tradeHitterData[currentPlayerIndices[j]];
+            // create a line if not null
+            if(innerHTMLStringArray != null) {
+                // update hitter head
+                rosterHitterHeadBody.innerHTML = rosterHitterHeadBody.innerHTML + '<tr><th scope="row">' + innerHTMLStringArray[0] + '</th><td>' + innerHTMLStringArray[1] + '</td><td>' + innerHTMLStringArray[2] + '</td>';
+                ///// update hitter main /////
+                innerHTMLString = '<tr><td scope="row">' + innerHTMLStringArray[3] + '</td><td>';
+                if(innerHTMLStringArray[4] >= 0) {
+                    innerHTMLString = innerHTMLString + '<span style="color:red;">&#9650;' + innerHTMLStringArray[4] + '</span></td><td>';
+                } else {
+                    innerHTMLString = innerHTMLString + '<span style="color:blue;">&#9660;' + (-1 * Number(innerHTMLStringArray[4])) + '</span></td><td>';
                 }
+                // slice main body data
+                innerHTMLStringArraySlice = innerHTMLStringArray.slice(hitterBodyIndexRange[0], hitterBodyIndexRange[1]+1);
+                // specify decimals
+                for(i=0; i<innerHTMLStringArraySlice.length; i++) {
+                    innerHTMLStringArraySlice[i] = Number(innerHTMLStringArraySlice[i]).toFixed(hitterBodyDataDecimals[i]);
+                }
+                // write line to main body
+                rosterHitterMainBody.innerHTML = rosterHitterMainBody.innerHTML + innerHTMLString + innerHTMLStringArraySlice.join('</td><td>') + '</td></tr>';
             }
         }
-
-        ///// update hitter tables /////
-        // initialize
-        rosterHitterDefaultBody.innerHTML   = '';
-        rosterHitterAdvancedBody.innerHTML  = '';
-        // create hitter data lines if not null
-        if(playerList.length > 0) {
-            for(j=0; j<playerList.length; j++) {
-                // read hitter data
-                innerHTMLStringArray = playerList[j];
-                // update default hitter roster table
-                rosterHitterDefaultBody.innerHTML = rosterHitterDefaultBody.innerHTML + createRosterLineDefault(innerHTMLStringArray);
-                ///// update advanced hitter roster table /////
-                innerHTMLString = '<tr><th scope="row">' + innerHTMLStringArray[0] + '</th><td>' + innerHTMLStringArray[1] + '</td><td>' + innerHTMLStringArray[2] + '</td><td>';
-                // specify decimals for each data
-                innerHTMLData = innerHTMLStringArray.slice(hitterAdvancedIndexRange[0], hitterAdvancedIndexRange[1]+1);
-                for(i=0; i<innerHTMLData.length; i++) {
-                    innerHTMLData[i] = Number(innerHTMLData[i]).toFixed(hitterAdvancedDataDecimals[i]);
-                }
-                // write line to advanced hitter roster table
-                innerHTMLString = innerHTMLString + innerHTMLData.join('</td><td>') + '</td></tr>';
-                rosterHitterAdvancedBody.innerHTML = rosterHitterAdvancedBody.innerHTML + innerHTMLString;
-            }
-        }
-
     }
-
-    // filter pitchers if pitcher is selected
     if(currentPosition == 2) {
-
-        // filter pitchers
-        for(i=0; i<rosterPitcherData.length; i++) {
-            // check team selection
-            if(currentTeamSelection.includes(rosterPitcherData[i][2])) {
-                playerList.push(rosterPitcherData[i]);
-            }
-        }
-
-        ///// update pitcher tables /////
-        // initialize
-        rosterPitcherDefaultBody.innerHTML  = '';
-        rosterPitcherAdvancedBody.innerHTML = '';
-        // create pitcher data lines if not null
-        if(playerList.length > 0) {
-            for(j=0; j<playerList.length; j++) {
-                // read pitcher data
-                innerHTMLStringArray = playerList[j];
-                // update default pitcher roster table
-                rosterPitcherDefaultBody.innerHTML = rosterPitcherDefaultBody.innerHTML + createRosterLineDefault(innerHTMLStringArray);
-                ///// update advanced pitcher roster table /////
-                innerHTMLString = '<tr><th scope="row">' + innerHTMLStringArray[0] + '</th><td>' + innerHTMLStringArray[1] + '</td><td>' + innerHTMLStringArray[2] + '</td><td>';
-                // specify decimals for each data
-                innerHTMLData = innerHTMLStringArray.slice(pitcherAdvancedIndexRange[0], pitcherAdvancedIndexRange[1]+1);
-                for(i=0; i<innerHTMLData.length; i++) {
-                    innerHTMLData[i] = Number(innerHTMLData[i]).toFixed(pitcherAdvancedDataDecimals[i]);                    
+        // update pitcher tables
+        for(j=0; j<currentPlayerIndices.length; j++) {
+            // read pitcher data
+            innerHTMLStringArray = tradePitcherData[currentPlayerIndices[j]];
+            // create a line if not null
+            if(innerHTMLStringArray != null) {
+                // update pitcher head
+                rosterPitcherHeadBody.innerHTML = rosterPitcherHeadBody.innerHTML + '<tr><th scope="row">' + innerHTMLStringArray[0] + '</th><td>' + innerHTMLStringArray[1] + '</td><td>' + innerHTMLStringArray[2] + '</td>';
+                ///// update pitcher main /////
+                innerHTMLString = '<tr><td scope="row">' + innerHTMLStringArray[3] + '</td><td>';
+                if(innerHTMLStringArray[4] >= 0) {
+                    innerHTMLString = innerHTMLString + '<span style="color:red;">&#9650;' + innerHTMLStringArray[4] + '</span></td><td>';
+                } else {
+                    innerHTMLString = innerHTMLString + '<span style="color:blue;">&#9660;' + (-1 * Number(innerHTMLStringArray[4])) + '</span></td><td>';
                 }
-                // write line to advanced pitcher roster table
-                innerHTMLString = innerHTMLString + innerHTMLData.join('</td><td>') + '</td></tr>';
-                rosterPitcherAdvancedBody.innerHTML = rosterPitcherAdvancedBody.innerHTML + innerHTMLString;
+                // slice main body data
+                innerHTMLStringArraySlice = innerHTMLStringArray.slice(pitcherBodyIndexRange[0], pitcherBodyIndexRange[1]+1);
+                // specify decimals
+                for(i=0; i<innerHTMLStringArraySlice.length; i++) {
+                    innerHTMLStringArraySlice[i] = Number(innerHTMLStringArraySlice[i]).toFixed(pitcherBodyDataDecimals[i]);
+                }
+                // write line to main body
+                rosterPitcherMainBody.innerHTML = rosterPitcherMainBody.innerHTML + innerHTMLString + innerHTMLStringArraySlice.join('</td><td>') + '</td></tr>';
             }
         }
-
     }
 }
-
-rosterHitterDefault.addEventListener("click", function() {
-    rosterHitterDefault.hidden = true;
-    rosterHitterAdvanced.hidden = false;
-})
-
-rosterPitcherDefault.addEventListener("click", function() {
-    rosterPitcherDefault.hidden = true;
-    rosterPitcherAdvanced.hidden = false;
-})
-
-rosterHitterAdvanced.addEventListener("click", function() {
-    rosterHitterDefault.hidden = false;
-    rosterHitterAdvanced.hidden = true;
-})
-
-rosterPitcherAdvanced.addEventListener("click", function() {
-    rosterPitcherDefault.hidden = false;
-    rosterPitcherAdvanced.hidden = true;
-})
 
 window.onload = function() {
 
-    updateTeamSelection('FA');
-    updatePosition(1);
-    updateHitterPositionSelection('All');
+    // updateTeamSelection('FA');
+    // updatePosition(2);
+    updateCurrentPlayerList();
+    writePlayerTable();
 }
